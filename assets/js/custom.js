2464348d9bcc1fce75815ce5d78cbd04f2f06732
veratech <jared@veratech.co.nz>
2018-05-01 21:36:40 +1200

    firstfornew

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// All custom js here - bundles to bundle.js
+(function(){
+
+	var Reveal = require('reveal.js');
+
+	//Initialize Reveal.js
+	Reveal.initialize({
+	  controls: false,
+	  progress: true,
+	  history: true,
+	  center: false,
+	  keyboard: true,
+	  scroll: true,
+	  transition: 'none',
+	  width: '95%',
+	  height: '95%'
+	});
+
+}());
