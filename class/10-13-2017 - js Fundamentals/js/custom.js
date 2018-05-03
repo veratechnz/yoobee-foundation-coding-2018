82591d21c8aa202ccfc3835f458fb99f1910d87e
veratech <jared@veratech.co.nz>
2018-05-03 17:02:36 +1200

    js-fundamentals

---
+++ class/10-13-2017 - js Fundamentals/js/custom.js
@@ -0,0 +1,18 @@
+// All custom js here - bundles to bundle.js
+(function(){
+
+	var Reveal = require('reveal.js');
+
+	//Initialize Reveal.js
+	Reveal.initialize({
+	  controls: false,
+	  progress: true,
+	  history: true,
+	  center: false,
+	  keyboard: true,
+	  transition: 'none',
+	  width: '95%',
+	  height: '95%'
+	});
+
+}());
