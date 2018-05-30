dbf09bcfbcb8912b11b00a710b2b370414d7a2c0
veratech <jared@veratech.co.nz>
2018-05-30 20:56:59 +1200

    latest

---
+++ loop-test.js
@@ -0,0 +1,20 @@
+// A. // 1 Create a function that copies all of the items (not prices) to a new array
+// 2 Then re-order the array alphabetically
+// 3 Try to make the function use an argument that references the array name
+
+// B. // 1 Create another function that takes all of the prices (not items) and assigns
+// them to a new myArray
+// 2. Re-order the new array numerically from lowest to highest
+// 3 Try to make the function use an argument that references the array name
+
+var shoppingCart = [
+  {item: `soap`, price: 4.99},
+  {item: `hammer`, price: 19.60},
+  {item: `tissues`, price: 2.95},
+  {item: `chocolates`, price: 5.95},
+  {item: `fruit`, price: 27.60},
+  {item: `oil`, price: 80.95},
+  {item: `tv`, price: 220.00},
+  {item: `phone`, price: 100.60},
+  {item: `meat`, price: 12.95}
+];
